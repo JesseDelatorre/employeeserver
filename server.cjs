9d8e3ce171d39f461c102c6071e8c702f9cbb7b1
Jesse <Existance@jesses-MacBook-Pro.local>
2025-02-17 13:40:57 -0500

    added if else statement

--- server.cjs
+++ server.cjs
@@ -1,6 +1,6 @@
 
 const employees = require('./employees.cjs');
-
+let idNumber = 5;
 const express = require('express');
 
 const app = express();
@@ -17,22 +17,47 @@ app.get('/employees', (req, res) => {
   res.send(employees);
 });
 
-app.get('/employees/:id', (req, res) => {
-  const {id} = resizeBy.params;
+// app.get('/employees/:id', (req, res) => {
+//   const {id} = resizeBy.params;
 
-  const foundEmployee = employees.find((individualEmployee) => {
-    return individualEmployee.id === Number(id);
-  })
-  res.send(foundEmployee);
-});
+//   const foundEmployee = employees.find((individualEmployee) => {
+//     return individualEmployee.id === Number(id);
+//   })
+//   res.send(foundEmployee);
+// });
 
 app.post('/api/v1/employees', (req, res, next) => {
+  console.log(req.body);
+  const { name } = req.body
+  if(!name) {
+    const error = new Error("Name not provided");
+    next(error);
+  } else {
+    employees.push({
+      id: idNumber,
+      name : name,
+     
+    });
+  
+    idNumber++;
+  
+    res.send(employees);
+  }
+});
 
-})
 
 
-app.use('*', (req, res) => {
-  res.send('404 Page Not Found');
+// app.use('*', (req, res) => {
+//   res.send('404 Page Not Found');
+// });
+
+app.use((err, req, res, next) => {
+  console.log('ERROR MESSAGE', err.message);
+  res.status(400).send(err.message);
+});
+
+app.use((req, res) => {
+  res.status(404).send('Page Not Found!')
 });
 
 const PORT = process.env.PORT || 3000; 
--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ const App = () => {
 
   return (
     <>
-      <h1>Welcome to the Store</h1>
+      <h1>Welcome Employees</h1>
     </>
   )
 }
